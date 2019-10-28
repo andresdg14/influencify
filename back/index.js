c550a5a1a2d1df6b2e46efac236dad8b5baa03f3
Andres <andresdomlez@gmail.com>
2019-10-28 12:40:40 +0000

    Proyecto base

--- back/index.js
+++ back/index.js
@@ -1,23 +1,24 @@
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const jwt = require('jsonwebtoken');
-
-const app = express();
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const jwt = require('jsonwebtoken')
+const app = express()
 
 // CONFIG AND ENVIRONMENT LOADING FROM .env FILE
-let config = require('./.env');
-const environment = process.env.NODE_ENV;
-config = config[environment];
+let config = require('./.env')
+const environment = process.env.NODE_ENV
+config = config[environment]
 if (!config) {
-  return console.error(`❌ ERROR | Invalid ${environment} environment`);
+  throw new Error(`❌ Invalid ${environment} environment`)
 }
 
-app.use(cors({ origin: 'http://localhost:8080'}));
-app.use(morgan('combined'));
-app.use(express.json());
-app.use(express.urlencoded( {extended: false}));
+// MIDDLEWARES
+app.use(cors())
+app.use(morgan('combined'))
+app.use(express.json())
+
+const UserModel = require('./models/users.model')
 
 const authenticate = (req, res, next) => {
   jwt.verify(req.headers.token, 'secret', (err, token) => {
@@ -26,47 +27,41 @@ const authenticate = (req, res, next) => {
         error: 'Token not valid'
       })
     }
-    res.locals.token = token;
-    next();
+    UserModel.findOne({
+        email: token.email
+      })
+      .then(user => {
+        res.locals.user = user
+        next()
+      })
   });
 }
 
 // NONGOOSE
-mongoose
-  .connect(config.mongoURL + config.mongoDBName, {
-    useNewUrlParser: true
-  })
-  .then(() => console.info('🗄️  MongoDB Connected'))
-  .catch(err => console.log(err));;
+mongoose.connect(config.mongoURL + config.mongoDBName, {
+  useNewUrlParser: true
+}, (err) => {
+  if (err) {
+    throw new Error(err)
+  }
+  console.info('💾 Mongoose is connected')
+})
 
 // ROUTING
-const apiRouter = require('./routes');
-app.use('/api', apiRouter);
+const apiRouter = require('./routes')
+app.use('/api', apiRouter)
 
-app.get('/api/login', authenticate, (req, res) => {
-  console.log(res.locals.token.email);
-  const UserModel = require('./models/users.model');
-  UserModel
-    .findOne({
-      email: res.locals.token.email
-    })
-    .then(user => {
-      res.send(`jalou! ${ user.name }`);
-    })
+app.get('/api/whoami', authenticate, (req, res) => {
+  res.send(`jalou! ${res.locals.user.name}`)
 })
 
 // Init server
 app.listen(config.port, (err) => {
-  console.info('.\n\n\n' + '*'.repeat(60))
-  console.info(`💻  Reboot Backend Server Live`);
-  console.info(`📡  PORT: http://localhost:${config.port}`);
-  console.info('*'.repeat(60) + '\n\n\n.');
+  if (err) {
+    throw new Error(err)
+  }
+  console.info('\n\n' + '>'.repeat(40))
+  console.info('💻  Reboot Server Live')
+  console.info(`📡  PORT: http://localhost:${config.port}`)
+  console.info('>'.repeat(40) + '\n\n')
 })
-
-// //Connect to the server
-// client.connect(function(err) {
-//   console.log("Connected succesfully to the server");
-//   const db = client.db(dbName);
-//   client.close();
-// })
-
